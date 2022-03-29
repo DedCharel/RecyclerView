300ca6c5dc6456dc8a22cb74063f15fa3a65731d
Daniil Shcherbakov <Charel@bk.ru>
2022-03-29 10:09:43 +0300

    Create model

---
+++ app/src/main/java/com/example/recyclerview/model/User.kt
@@ -0,0 +1,10 @@
+package com.example.recyclerview.model
+
+data class User(
+    val id: Long,
+    val photo: String,
+    val name: String,
+    val company: String
+) {
+
+}
---
+++ app/src/main/java/com/example/recyclerview/model/UserService.kt
@@ -0,0 +1,69 @@
+package com.example.recyclerview.model
+
+import com.github.javafaker.Faker
+import java.util.*
+
+typealias UsersListener = (users: List<User>) -> Unit
+
+class UserService {
+    private val users = mutableListOf<User>()
+
+    private val listeners = mutableSetOf<UsersListener>()
+
+    init {
+        val faker = Faker.instance()
+        IMAGES.shuffle()
+        val generatedUsers = (1..100).map { User(
+            id = it.toLong(),
+            name = faker.name().name(),
+            company = faker.company().name(),
+            photo = IMAGES[it % IMAGES.size]
+        ) }
+    }
+
+    fun getUsers(): List<User>{
+        return users
+    }
+
+    fun deleteUser(user: User){
+        val indexDelete = users.indexOfFirst {it.id == user.id}
+        if (indexDelete != -1){
+            users.removeAt(indexDelete)
+            notifyChanges()
+        }
+    }
+
+    fun moveUser(user: User, moveBy: Int){
+        val oldIndex = users.indexOfFirst { it.id == user.id}
+        if (oldIndex == -1) return
+        val newIndex = oldIndex + moveBy
+        if (newIndex < 0 || newIndex >= users.size) return
+        Collections.swap(users, oldIndex, newIndex)
+        notifyChanges()
+    }
+
+    fun addListener(listener: UsersListener){
+        listeners.add(listener)
+        listener.invoke(users)
+    }
+
+    fun removeListener(listener: UsersListener){
+        listeners.remove(listener)
+    }
+
+    private fun notifyChanges(){
+        listeners.forEach{it.invoke(users)}
+    }
+
+    companion object{
+        private val IMAGES = mutableListOf(
+            "https://android-obzor.com/wp-content/uploads/2022/02/1-6.jpg",
+            "https://android-obzor.com/wp-content/uploads/2022/02/2-6.jpg",
+            "https://android-obzor.com/wp-content/uploads/2022/02/3-6.jpg",
+            "https://android-obzor.com/wp-content/uploads/2022/02/4-6.jpg",
+            "https://android-obzor.com/wp-content/uploads/2022/02/5-6.jpg",
+            "https://android-obzor.com/wp-content/uploads/2022/02/6-6.jpg",
+            "https://android-obzor.com/wp-content/uploads/2022/02/8-6.jpg"
+        )
+    }
+}
